7b13bb15c6fca0614839c1df031b674ed6753b36
kristianlaw <lawkristian@hotmail.com>
2020-11-24 23:18:02 +0200

    Discord-testailua

--- App.test.js
+++ App.test.js
@@ -2,20 +2,21 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import App from './App';
 //Source: https://docs.expo.io/guides/testing-with-jest/
-
+//Uses Jest to run test cases
+//npm run test
 
 
 
 //Our test will be the expected state of the <App /> to have 1 child element:
  describe('<App />', () => {
-  it('has 1 child', () => {
+  it('1 child within', () => {
     const tree = renderer.create(<App />).toJSON();
     expect(tree.children.length).toBe(1);
   });
 });
 
 //Snapshot testing
-it('renders correctly', () => {
+it('proper rendering', () => {
   const tree = renderer.create(<App />).toJSON();
   expect(tree).toMatchSnapshot();
 });
