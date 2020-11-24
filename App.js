8c99cf258ea71047eff867e215fcb03c16ac7e8f
kristianlaw <lawkristian@hotmail.com>
2020-11-24 17:38:18 +0200

    Discord testi

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+//Discord testi push
 export default function App() {
   return (
     <View style={styles.container}>
